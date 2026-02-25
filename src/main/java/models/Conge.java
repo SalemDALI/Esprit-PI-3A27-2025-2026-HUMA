6e30f6f54839c9b9a8d067aa469751c1dc8e71a6
dali <salemdali@outlook.fr>
2026-02-25 23:04:06 +0100

    app 26

---
+++ src/main/java/models/Conge.java
@@ -0,0 +1,105 @@
+package models;
+
+import java.time.LocalDate;
+
+public class Conge {
+    private int id;
+    private int absenceId;
+    private int employeId;
+    private String employeNom;
+    private LocalDate dateDebut;
+    private LocalDate dateFin;
+    private String statut;
+    private LocalDate dateDemande;
+    private Integer managerId;
+    private String commentaireValidation;
+    private LocalDate dateValidation;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAbsenceId() {
+        return absenceId;
+    }
+
+    public void setAbsenceId(int absenceId) {
+        this.absenceId = absenceId;
+    }
+
+    public int getEmployeId() {
+        return employeId;
+    }
+
+    public void setEmployeId(int employeId) {
+        this.employeId = employeId;
+    }
+
+    public String getEmployeNom() {
+        return employeNom;
+    }
+
+    public void setEmployeNom(String employeNom) {
+        this.employeNom = employeNom;
+    }
+
+    public LocalDate getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(LocalDate dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public LocalDate getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(LocalDate dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public String getStatut() {
+        return statut;
+    }
+
+    public void setStatut(String statut) {
+        this.statut = statut;
+    }
+
+    public LocalDate getDateDemande() {
+        return dateDemande;
+    }
+
+    public void setDateDemande(LocalDate dateDemande) {
+        this.dateDemande = dateDemande;
+    }
+
+    public Integer getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(Integer managerId) {
+        this.managerId = managerId;
+    }
+
+    public String getCommentaireValidation() {
+        return commentaireValidation;
+    }
+
+    public void setCommentaireValidation(String commentaireValidation) {
+        this.commentaireValidation = commentaireValidation;
+    }
+
+    public LocalDate getDateValidation() {
+        return dateValidation;
+    }
+
+    public void setDateValidation(LocalDate dateValidation) {
+        this.dateValidation = dateValidation;
+    }
+}
